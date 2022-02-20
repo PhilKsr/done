56eb91551b19573b9b706b0f1122edf602f568b0
Philipp Kaiser <p.kaiser95@gmx.de>
2022-02-20 21:47:32 +0100

    Added TodoList component with functions

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,17 +1,37 @@
 import { SetStateAction } from "react";
 import { Todo } from "../lib/model";
+import SingleTodo from "./SingleTodo";
 
 interface Props {
   todos: Todo[];
-  onSetTodos: React.Dispatch<SetStateAction<string>>;
+  onSetTodos: React.Dispatch<SetStateAction<Todo[]>>;
 }
 
 const TodoList = ({ todos, onSetTodos }: Props) => {
   return (
     <div>
-      {todos.map((t) => (
-        <li>{t.todo}</li>
-      ))}
+      <div>
+        <span>Active Tasks</span>
+        {todos.map((t) => (
+          <SingleTodo
+            key={t.id}
+            todo={t}
+            todos={todos}
+            onSetTodos={onSetTodos}
+          />
+        ))}
+      </div>
+      <div>
+        <span>Completed Tasks</span>
+        {todos.map((t) => (
+          <SingleTodo
+            key={t.id}
+            todo={t}
+            todos={todos}
+            onSetTodos={onSetTodos}
+          />
+        ))}
+      </div>
     </div>
   );
 };
